98a0580b4801b2cbbb5b6b351262a70afaa65c46
glampropo <glampropoulos@epu.ntua.gr>
2024-02-14 13:35:52 +0200

    update assignments endpoint

--- load_forecasting/load_forecasting/resources/flexdr_service.py
+++ load_forecasting/load_forecasting/resources/flexdr_service.py
@@ -11,10 +11,10 @@ class FlexDRService(ConfigurableResource):
                          forecast_date: str) -> Response:
         payload = {
             'meter_id': device_id,
-            'ml_model_id': '64f99f5dd7dccbfc89e01ef2',
+            'ml_model_id': '65c77636ca1fd6e7931b21af',
             'cluster_assigned': cluster,
             'forecasted_load': day_ahead_load,
             'forecast_date': forecast_date
         }
-        response = requests.post(url=f'{self.endpoint}/assignments/new', json=payload)
+        response = requests.post(url=f'{self.endpoint}/assignments/assignment', json=payload)
         return response
